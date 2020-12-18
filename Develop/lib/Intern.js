28c6a7fecf9ccb8dc21b1695b085400e84fa4a24
jonnahmarie <jonnah.marie@gmail.com>
2020-12-18 00:01:37 -0800

    testing code

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -4,12 +4,11 @@ const Employee = require("./Employee");
 class Intern extends Employee {
     constructor(name, id, email, school) {
         super(name, id, email);
-
         this.school = school;
     }
 
     getSchool() {
-        return this.School;
+        return this.school;
     }
 }
 
